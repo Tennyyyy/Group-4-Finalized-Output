42d179f6866fb45c99ba6150b6bffcdbaecbead4
Tennyyyy <114280649+Tennyyyy@users.noreply.github.com>
2025-03-16 23:00:57 +0800

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 $(document).ready(function(){
 
-    // jQuery methods go here...
+ $(document).ready(function () {
+
+            // Welcome section fade in
+            $('.welcome-text').hide().fadeIn(2000);
+
+            // Slide down navigation links on hover
+            $('.nav-links li').hover(function () {
+                $(this).stop().slideDown(300);
+            }, function () {
+                $(this).stop().slideUp(300);
+            });
+
+            // Image gallery hover effect
+            $('.gallery-item').hover(
+                function () {
+                    $(this).find('.gallery-overlay').fadeIn(300);
+                },
+                function () {
+                    $(this).find('.gallery-overlay').fadeOut(300);
+                }
+            );
+
+            // Quote section fade in
+            $('.quote').hide().fadeIn(2500);
+
+        });
   
-  });
+});
